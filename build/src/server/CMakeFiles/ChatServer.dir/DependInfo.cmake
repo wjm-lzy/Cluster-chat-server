a0d6f1ed9d6f6c97b28a00f55bf8d0b211ce76e2
wjm-lzy <3635836054@qq.com>
2025-04-24 07:21:34 -0400

    wjm

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wjm/projects/MyProject__server/src/server/chatserver.cpp" "/home/wjm/projects/MyProject__server/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/home/wjm/projects/MyProject__server/src/server/chatservice.cpp" "/home/wjm/projects/MyProject__server/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/home/wjm/projects/MyProject__server/src/server/db/db.cpp" "/home/wjm/projects/MyProject__server/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/home/wjm/projects/MyProject__server/src/server/main.cpp" "/home/wjm/projects/MyProject__server/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/wjm/projects/MyProject__server/src/server/model/friendmoel.cpp" "/home/wjm/projects/MyProject__server/build/src/server/CMakeFiles/ChatServer.dir/model/friendmoel.cpp.o"
+  "/home/wjm/projects/MyProject__server/src/server/model/groupmodel.cpp" "/home/wjm/projects/MyProject__server/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/home/wjm/projects/MyProject__server/src/server/model/offlinemessagemodel.cpp" "/home/wjm/projects/MyProject__server/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/home/wjm/projects/MyProject__server/src/server/model/usermodel.cpp" "/home/wjm/projects/MyProject__server/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/home/wjm/projects/MyProject__server/src/server/redis/redis.cpp" "/home/wjm/projects/MyProject__server/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
